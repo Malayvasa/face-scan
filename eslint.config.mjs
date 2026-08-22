7783df411ff5c3015263e18a57d88d1795d4e05d
Malay Vasa <malayvasa@gmail.com>
2026-08-22 17:22:17 +0530

    chore: add Next.js, TypeScript, and package config

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "public/face-scan/load-vision.js",
+  ]),
+  {
+    rules: {
+      "react-hooks/refs": "off",
+    },
+  },
+]);
+
+export default eslintConfig;
